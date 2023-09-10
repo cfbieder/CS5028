5881f6f8249ec67c4faf631fcdebac8deca16668
Chris Biedermann <cfbieder@hotmail.com>
2023-09-10 12:32:57 +0200

    check change

--- data_extractor/app.js
+++ data_extractor/app.js
@@ -4,7 +4,10 @@ async function main() {
     console.log("Getting RSS Feed");
     let feed = await helper.rssGetFeed();
     console.log(`Items retreived: ${feed.length}`);
+
+    console.log("Starting connection to RabbitMQ")
 }
+
 
 main()
 
